f0a52d777f674168d43f8b5fff770025b61fb50c
Darius Jazayeri <jazayeri@alum.mit.edu>
2014-10-23 16:44:08 +0530

    Initial commit

---
+++ api/src/test/java/org/openmrs/module/orderentryui/OrderEntryUiActivatorTest.java
@@ -0,0 +1,14 @@
+package org.openmrs.module.orderentryui;
+
+import org.junit.Test;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+
+import static org.junit.Assert.assertTrue;
+
+public class OrderEntryUiActivatorTest extends BaseModuleContextSensitiveTest {
+
+    @Test
+    public void testContext() throws Exception {
+        assertTrue(true);
+    }
+}
